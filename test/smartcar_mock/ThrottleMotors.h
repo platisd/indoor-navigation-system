db11426c300e01f0afcc8d2619716eec11c888dc
platisd <kurukii88@gmail.com>
2017-01-09 01:13:02 +0100

    Revert "Revert "Adding motor mocks""
    This reverts commit d5361462667fffe49264741f93b5995256942272.

--- test/smartcar_mock/ThrottleMotors.h
+++ test/smartcar_mock/ThrottleMotors.h
@@ -1,4 +1,39 @@
 #ifndef THROTTLE_MOTOR_H
 #define THROTTLE_MOTOR_H
 
+class ThrottleMotorMock {
+public:
+    MOCK_METHOD1(setSpeed, void(int));
+    MOCK_METHOD2(setMotorSpeed, void(int,int));
+};
+
+/* Methods for instantiating and deleting the mock */
+static ThrottleMotorMock* throttleMotorMock = NULL;
+ThrottleMotorMock* throttleMotorMockInstance() {
+  if(!throttleMotorMock) {
+    throttleMotorMock = new ThrottleMotorMock();
+  }
+  return throttleMotorMock;
+}
+
+void releaseThrottleMotorMock() {
+  if(throttleMotorMock) {
+    delete throttleMotorMock;
+    throttleMotorMock = NULL;
+  }
+}
+
+/* ------------------------------------------------ */
+class ThrottleMotor {
+public:
+    ThrottleMotor(){};
+    ~ThrottleMotor(){};
+    void setSpeed(int speed){
+        throttleMotorMock->setSpeed(speed);
+    };
+    void setMotorSpeed(int leftMotorSpeed, int rightMotorSpeed){
+        throttleMotorMock->setMotorSpeed(leftMotorSpeed, rightMotorSpeed);
+    };
+};
+
 #endif // THROTTLE_MOTOR_H
