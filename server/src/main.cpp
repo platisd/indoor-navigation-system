75ccad87857463531ea502f2824eff173c31c9ab
Samuel Idowu <samuel.idowu@delphi.com>
2017-10-27 22:32:52 +0200

    Refactoring ...
    * Update header guards
    * Change code format based on new .clang-format
    * Use one logger instance instead of one per class
    * Declare UT classes as friend class
    * Related to #15

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,20 +4,23 @@
 
 #include "ins_service.hpp"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     spdlog::set_level(spdlog::level::debug);
 
     Pistache::Port port(9080);
-    int thread_count = 2;
+    int            thread_count = 2;
 
-    if (argc >= 2) {
+    if (argc >= 2)
+    {
         port = static_cast<uint16_t>(std::stol(argv[1]));
 
-        if (argc == 3) thread_count = static_cast<int>(std::stol(argv[2]));
+        if (argc == 3)
+            thread_count = static_cast<int>(std::stol(argv[2]));
     }
 
     Pistache::Address addr(Pistache::Ipv4::any(), port);
-    auto console = spdlog::stdout_logger_mt("main_console");
+    auto              console = spdlog::stdout_logger_mt("main_console");
 
     console->info("Indoor Navigation System service setting up on {1}:{0:d}", addr.port(), addr.host());
     console->info("Cores = {0}", Pistache::hardware_concurrency());
