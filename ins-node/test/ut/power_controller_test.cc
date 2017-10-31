0393ff1fb39ab360ace4c226c340731e6b7d19c2
Dimitris Platis <kurukii88@gmail.com>
2017-10-31 22:55:24 +0100

    Separate source folders for ins-node
    * Power controller MCU and Wifi module sketches placed
    in separate folders
    * Updates tests
    * Renamed test_utils.h
    
    This new structure is additionally more on par with how
    Arduino IDE handles sketches (i.e. sketch_name/sketch_name.ino)
    and makes it easier to figure out which code should run where.
    
    Fixes #21

--- ins-node/test/ut/power_controller_test.cc
+++ ins-node/test/ut/power_controller_test.cc
@@ -1,38 +1,39 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "arduino-mock/Arduino.h"
-#include "test_utils.h"
+#include "power_controller_utils.h"
 
-#include "../../src/ins_node.ino"
+#include "../../src/power_controller/power_controller.ino"
 
 using ::testing::Return;
 
-class InsNodeFixture : public ::testing::Test
+class PowerControllerFixture : public ::testing::Test
 {
    public:
-     ArduinoMock* arduinoMock;
+    ArduinoMock* arduinoMock;
 
     // Run this before the tests
     virtual void SetUp()
     {
-      arduinoMock = arduinoMockInstance();
+          arduinoMock = arduinoMockInstance();
     }
     // Run this after the tests
     virtual void TearDown()
     {
-      releaseArduinoMock();
+          releaseArduinoMock();
     }
 };
 
 
-TEST_F(InsNodeFixture, insNodeSetup_whenWifiIsTurnedOn_willNotCrash)
+TEST_F(PowerControllerFixture, powerControllerSetup_whenWifiIsTurnedOn_willNotCrash)
 {
   uint8_t expected_error = 0;
   EXPECT_CALL(*arduinoMock, digitalWrite(WIFI_MODULE_PIN, HIGH));
   ASSERT_EQ(turnWifi(HIGH), expected_error);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
