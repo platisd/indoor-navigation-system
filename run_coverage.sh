2f4fbe578fb8808993fe3fa9bca68ed9a18dee00
Dimitris Platis <kurukii88@gmail.com>
2017-11-19 18:00:22 +0100

    Add coverage reports for server

--- run_coverage.sh
+++ run_coverage.sh
@@ -4,6 +4,7 @@ caller_dir=$PWD
 cd "${0%/*}"
 
 ins_node_test_dir="ins-node/test"
+ins_server_dir="server"
 
 # Specify which directories are to be excluded from coverage report
 coverage_ignore_dirs="\
@@ -11,7 +12,9 @@ coverage_ignore_dirs="\
     '${ins_node_test_dir}/ins_mock/*' \
     '${ins_node_test_dir}/build/arduino_mock/*' \
     '${ins_node_test_dir}/stubs/*' \
-    '${ins_node_test_dir}/ut/*'
+    '${ins_node_test_dir}/ut/*' \
+    '${ins_server_dir}/include/external/spdlog/*' \
+    '${ins_server_dir}/test/*'
 "
 
 eval lcov --directory . --capture --output-file coverage.info
