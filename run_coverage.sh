e95b0f96f53b57e77378a1aaaa6f819dc9e5f83c
Dimitris Platis <kurukii88@gmail.com>
2017-11-19 17:31:31 +0100

    Add lcov initial structure
    * Currently reports for ins-node
    * Missing uploading to codecov.io
    * Missing reports for server

---
+++ run_coverage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+caller_dir=$PWD
+# Move to the current script directory
+cd "${0%/*}"
+
+ins_node_test_dir="ins-node/test"
+
+# Specify which directories are to be excluded from coverage report
+coverage_ignore_dirs="\
+    '/usr/*' \
+    '${ins_node_test_dir}/ins_mock/*' \
+    '${ins_node_test_dir}/build/arduino_mock/*' \
+    '${ins_node_test_dir}/stubs/*' \
+    '${ins_node_test_dir}/ut/*'
+"
+
+eval lcov --directory . --capture --output-file coverage.info
+eval lcov --remove coverage.info $coverage_ignore_dirs --output-file coverage.info
+eval lcov --list coverage.info
+
+# Go back to the initial directory when you are done
+cd $caller_dir
