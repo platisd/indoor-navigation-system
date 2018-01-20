fd7c3bea024003131d64321c604b199135e273c6
Dimitris Platis <platisd@users.noreply.github.com>
2018-01-20 21:30:37 +0100

    Add more stubs and fakes

--- server/test/mocks/pistache/mock_pistache.hpp
+++ server/test/mocks/pistache/mock_pistache.hpp
@@ -4,6 +4,10 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+#include <functional>
+#include <string>
+#include <vector>
+
 class PistacheMock
 {
 public:
@@ -39,40 +43,132 @@ int hardware_concurrency()
 class Port
 {
 public:
-    Port(uint16_t) = default;
+    Port(uint16_t){}
 };
 
 class Ipv4
 {
-    Ipv4(uint8_t, uint8_t, uint8_t, uint8_t) = default;
+    Ipv4(uint8_t, uint8_t, uint8_t, uint8_t){}
     static Ipv4 any();
 };
 
 class Address
 {
-    Address(Ipv4, Port) = default;
+    Address(Ipv4, Port){}
 };
 
 namespace Tcp
 {
 enum class Options
 {
-    InstallSignalHandler = 0
+    InstallSignalHandler = 0,
 };
 } // Tcp
 
+namespace Http
+{
+
+enum class Code
+{
+    Ok = 0,
+    Internal_Server_Error = 1,
+};
+
+class Cookie
+{
+public:
+    Cookie(std::string n, std::string v) : name(n), value(v) {}
+    std::string name, value;
+};
+
+class CookieJar
+{
+public:
+    void add(Pistache::Http::Cookie);
+};
+
+class ResponseWriter
+{
+public:
+    ResponseWriter(){}
+    void send(Pistache::Http::Code);
+    void send(Pistache::Http::Code,  std::string);
+    CookieJar cookies() const
+    {
+        return CookieJar();
+    }
+
+};
+
+class Endpoint
+{
+public:
+    Endpoint(Pistache::Address){}
+    class Options
+    {
+    public:
+        Options& threads(int);
+        Options& flags(Tcp::Options);
+    };
+    static Options options();
+};
+} // Http
+
 namespace Rest
 {
+
+namespace details {
+    template<typename T> struct LexicalCast {
+        static T cast(const std::string& value) {
+            std::istringstream iss(value);
+            T out;
+            if (!(iss >> out))
+                throw std::runtime_error("Bad lexical cast");
+            return out;
+        }
+    };
+
+    template<>
+    struct LexicalCast<std::string> {
+        static std::string cast(const std::string& value) {
+            return value;
+        }
+    };
+} // details
+
+
+class TypedParam
+{
+public:
+    TypedParam(std::string p = "") : parameter_(p) {}
+    template<typename T> T as() const
+    {
+        return details::LexicalCast<T>::cast(parameter_);
+    }
+private:
+    std::string parameter_;
+};
+
 class Request
 {
 public:
-    Request() = default;
+    Request() {}
+    bool hasParam(std::string name) const;
+    TypedParam param(std::string p) const
+    {
+        return TypedParam(p);
+    }
+
+    std::vector<Pistache::Http::Cookie> cookies() const
+    {
+        return {Pistache::Http::Cookie("", "")};
+    }
 };
 
 class Router
 {
 public:
-    Router() = default;
+    Router(){}
 };
 
 namespace Routes
@@ -82,49 +178,35 @@ namespace Route
 class Handler
 {
 public:
-    Handler() = default;
+    Handler(){}
 };
+
+
+enum class Result
+{
+    Ok = 0,
+};
+
 } // Route
     void Get(Router& router, std::string resource, Route::Handler handler);
     void Post(Router& router, std::string resource, Route::Handler handler);
-    // template<typename Result, typename... Args>
-    // Route::Handler bind(Result (*func)(Args...)) {
-    // }
+
+    template<typename Result, typename Cls, typename... Args, typename Obj>
+    Route::Handler bind(Result (Cls::*func)(Args...), Obj obj) {
+        #define CALL_MEMBER_FN(obj, pmf)  ((obj)->*(pmf))
+
+        return [=](const Rest::Request& request, Http::ResponseWriter response) {
+            CALL_MEMBER_FN(obj, func)(request, std::move(response));
+            // Create mock method RestCallback(std::function<void(Rest::Request&, Http::ResponseWriter)>)
+            // And call it here
+            return Route::Result::Ok;
+        };
+
+        #undef CALL_MEMBER_FN
+    }
+
 } // Routes
 } // Rest
-
-namespace Http
-{
-class ResponseWriter
-{
-public:
-    ResponseWriter() = default;
-};
-
-class Endpoint
-{
-public:
-    Endpoint() = default;
-    class Options
-    {
-        Options& threads(int);
-        Options& flags(Tcp::Options);
-    };
-    static Options options();
-};
-
-enum class Code
-{
-    Ok = 0
-    Internal_Server_Error = 1
-};
-
-class Cookie
-{
-    Cookie(std::string, std::string) = default;
-};
-} // Http
-
 } // Pistache
 
 #endif
