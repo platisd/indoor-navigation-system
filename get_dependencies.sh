400c410c52c9fa1564485bd6eb8056efebbb1cb3
Samuel Idowu <samup4web@yahoo.com>
2018-01-22 18:41:47 +0100

    Extend get_dependency script to install cmake 3.10

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -13,5 +13,17 @@ mkdir build && cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 make
 sudo make install
+
+# Install cmake 3.10
+sudo apt-get purge cmake
+version=3.10
+build=2
+mkdir ~/temp && cd ~/temp
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz && cd cmake-$version.$build/
+./bootstrap
+make -j4
+sudo make install
+
 # Go back to the initial directory when you are done
 cd $caller_dir
