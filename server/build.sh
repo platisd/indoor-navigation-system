9735953272f222a8f0e788dbeba49bc11be5d221
Samuel Idowu <samup4web@yahoo.com>
2018-01-22 21:06:36 +0100

    Fix server/build.sh to propagate exit status to bash
    * Without this fix, Travis reports success even when
      server/build.sh fails

--- server/build.sh
+++ server/build.sh
@@ -8,7 +8,7 @@ cd "${0%/*}"
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_CXX_CLANG_TIDY:STRING="clang-tidy;-checks=*;-warnings-as-errors=*,-*-pointer-arithmetic" ..
-cmake --build .
+cmake -DCMAKE_CXX_CLANG_TIDY:STRING="clang-tidy;-checks=*;-warnings-as-errors=*,-*-pointer-arithmetic" .. || exit 1 
+cmake --build . || exit 1
 # Go back to the initial directory when you are done
 cd $caller_dir
